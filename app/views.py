a7fbf884ed85317308758634f508c1522b6c7f40
davy1ex <dezy1ex@gmail.com>
2019-05-11 10:55:47 +0400

    refactoring

--- app/views.py
+++ app/views.py
@@ -1,12 +1,10 @@
-# ДОБАВИТЬ ОГРАНИЧЕНИЯ НА ВВОД В КОММЕНТАХ
-# ДОБАВИТЬ УДАЛЕНИЕ ФОТО ОТ УДАЛЁННЫХ ПОСТОВ/НОРМАЛЬНУЮ СИСТЕМУ ХРАНЕНИЯ ФОТО
 import os
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 
 from app import app, db
 from app.models import User, Post, Comment, Answer
-from app.forms import RegForm, LoginForm, ProfileSettingsForm, TextEditorForm, AddCommentForm
+from app.forms import LoginForm, ProfileSettingsForm, TextEditorForm, AddCommentForm
 
 admin_username = "admin"
 
@@ -64,14 +62,7 @@ def add_post():
     form = TextEditorForm()
     if form.validate_on_submit():
 
-        if form.text != "":
-            post = Post(master=current_user, body=form.text.data, title=form.title.data)
-
-            db.session.add(post)
-            db.session.commit()
-            post = Post.query.filter_by(title=form.title.data).first()
-            os.mkdir(os.path.join(os.getcwd(), "app", "static", "img", str(post.id)))
-
+        if Validator.data_required(form.text.data):
             if "file" not in request.files:
                 return redirect(url_for("add_post"))
 
@@ -79,10 +70,24 @@ def add_post():
 
             if file.filename == "":
                 return redirect(url_for("add_post"))
-  
-            file.save(os.path.join(os.getcwd(), "app", "static", "img", str(post.id), "img.jpeg"))
 
-            return redirect(url_for("index"))
+            if "png" in file.filename or "jpeg" in file.filename or "jpg" in file.filename:
+                post = Post(master=current_user, body=form.text.data, title=form.title.data)
+
+                db.session.add(post)
+                db.session.commit()
+
+                post = Post.query.filter_by(title=form.title.data).first()
+
+                path_save = os.path.join(os.getcwd(), "app", "static", "img", str(post.id))
+
+                if os.path.isdir(path_save):
+                    os.system("rm -rf %s" % path_save)
+
+                os.mkdir(os.path.join(os.getcwd(), "app", "static", "img", str(post.id)))
+                file.save(os.path.join(path_save, "img.jpeg"))
+
+                return redirect(url_for("index"))
 
     return render_template("editor.html", form=form)
 
@@ -91,7 +96,7 @@ def add_post():
 @app.route("/remove_post/<id>")
 def remove_post(id):
     post = Post.query.filter_by(id=id).first()
-    os.system("rm -rf " + os.path.join("app", "static", "img", id))
+    os.system("rm -rf %s" % os.path.join("app", "static", "img", id))
     db.session.delete(post)
     db.session.commit()
     return redirect(url_for("index"))
@@ -107,9 +112,7 @@ def post(id):
     answer = False  # если тру, то автоставка никнейма для ответа
 
     if form.validate_on_submit():
-        # return str(form.body.data.split(" ")[0][1:-1])
         if "[" in form.body.data.split(" ")[0] and "]" in form.body.data.split(" ")[0] and Validator.email(form.email.data) and Validator.data_required(form.author.data):
-            # comment = Comment.query.filter_by(author=form.body.data.split(" ")[0][1:-1]).first()
             comment = Comment.query.filter_by(id=form.body.data.split(" ")[0][1:-1]).first()
             body = form.body.data.split("]")[1:][0]
             author = form.author.data
@@ -122,7 +125,6 @@ def post(id):
             return redirect(url_for("post", id=id))
 
         elif "reply" in request.form["submit"]:
-            # username = Comment.query.filter_by(id=int(request.form["submit"][9:])).first().author
             comment_id = request.form["submit"][9:]
             form.body.data = "[" + comment_id + "] "
             return render_template("index/post.html", post=post, form=form, comments=comments, answers=answers)
